59377ceb2ba9f24b9adadb3e50c9d64556c71e6f
Mehdi <ezka.mehdi@gmail.com>
2021-05-28 12:29:19 +0200

    etape 2 moitié

--- chapitre_02/heros/server.js
+++ chapitre_02/heros/server.js
@@ -1,6 +1,7 @@
-import express from 'express';
+const express = require("express")
+const cors = require("cors");
+const {superHeros} = require("./dataHeroes")
 const app = express();
-import cors from 'cors';
 
 
 app.use(express.json())
@@ -13,47 +14,35 @@ app.listen(port, () => {
 
 
 
-const debug = (req, res, next) => {
-    console.log("Server request")
-    next()
-}
-const superHeros = [
-    {
-        name: "Iron Man",
-        power: ["money"],
-        color: "red",
-        isAlive: true,
-        age: 46,
-        image: "https://blog.fr.playstation.com/tachyon/sites/10/2019/07/unnamed-file-18.jpg?resize=1088,500&crop_strategy=smart"
-    },
-    {
-        name: "Thor",
-        power: ["electricty", "worthy"],
-        color: "blue",
-        isAlive: true,
-        age: 300,
-        image: "https://www.bdfugue.com/media/catalog/product/cache/1/image/400x/17f82f742ffe127f42dca9de82fb58b1/9/7/9782809465761_1_75.jpg"
-    },
-    {
-        name: "Daredevil",
-        power: ["blind"],
-        color: "red",
-        isAlive: false,
-        age: 30,
-        image: "https://aws.vdkimg.com/film/2/5/1/1/251170_backdrop_scale_1280xauto.jpg"
-    }
-]
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now());
+    next();
+  });
+  
+  
+//   app.get("/heroes", (req, res) => {
+//       res.json(superHeros)
+//   })
 
-app.get("/heroes/:name/powers", (req, res) => {
-    res.json(superHeros)
-})
-
-app.post("/heroes/", (req, res) => {
-    const superHeros = req.body
-    res.send('ok,heros ajouté');
+let name = req.params.name;
+let powers = req.params.power;
+let color = req.params.color;
+let isAlive = req.params.isAlive;
+let age = req.params.age;
+let image = req.params.image;
+app.get("/heroes/:name", (req, res) => {
+    res.json({
+        message: `Le Heros :${name,powers,color,isAlive,age,image}!`,
+    });
 });
 
-const transformName = (req, res, next) => {
-    
-}
+app.get("/heroes/:name/powers", (req, res) => {
+    res.json({
+        message: `Hello ${powers}!`,
+    });
+})
+
+// app.post("/heroes", (req, res) => {
+//     console.log("Ok, heros ajouté !")
+// })
 
