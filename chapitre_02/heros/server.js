a8686e68a4e05f2aa937aeae33f68be722f9575f
Mehdi <ezka.mehdi@gmail.com>
2021-06-08 17:14:32 +0200

    bonus data

--- chapitre_02/heros/server.js
+++ chapitre_02/heros/server.js
@@ -1,6 +1,59 @@
 const express = require("express")
-const cors = require("cors");
+const cors = require("cors")
+const mongoose = require("mongoose")
 const {superHeros} = require("./dataHeroes")
+
+mongoose.connect("mongodb://localhost:27017/heros", (err) => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log("I'm connected to the database");
+    }
+})
+
+const herosSchema = mongoose.Schema({
+    name: String,
+    power: Array,
+    color: String,
+    isAlive: Boolean,
+    age: Number,
+    image:String,
+    date: { type: Date, default: Date.now }
+})
+
+const Heros = mongoose.model("Heros", herosSchema)
+
+// Heros.insertMany([
+//     {name: "Iron Man",
+//     power: ["money"],
+//     color: "red",
+//     isAlive: true,
+//     age: 46,
+//     image: "https://blog.fr.playstation.com/tachyon/sites/10/2019/07/unnamed-file-18.jpg?resize=1088,500&crop_strategy=smart"
+// },
+// {
+//     name: "Thor",
+//     power: ["electricty", "worthy"],
+//     color: "blue",
+//     isAlive: true,
+//     age: 300,
+//     image: "https://www.bdfugue.com/media/catalog/product/cache/1/image/400x/17f82f742ffe127f42dca9de82fb58b1/9/7/9782809465761_1_75.jpg"
+// },
+// {
+//     name: "Daredevil",
+//     power: ["blind"],
+//     color: "red",
+//     isAlive: false,
+//     age: 30,
+//     image: "https://aws.vdkimg.com/film/2/5/1/1/251170_backdrop_scale_1280xauto.jpg"
+//     },
+
+// ]).then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.error("Error insertMany Card: ", err);
+// })
+
 
 const port = 8000
 
@@ -113,4 +166,6 @@ app.post("/heroes/:name/powers", (req, res) => {
 app.listen(port, () => {
     console.log("Server is listenin at port ", port);
 })
+
+
 
