ae64e7383ae6f6752b72d322436179a43e83bb62
Mehdi <ezka.mehdi@gmail.com>
2021-06-09 17:24:39 +0200

    exo trippy route

---
+++  trippy_basics_api/db.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const cors = require("cors")
+
+const port = 8000
+
+const app = express()
+
+const debug = (req, res, next) => {
+    console.log("I received a request!");
+
+    next()
+}
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(debug)
+
+app.listen(port, () => {
+    console.log("Server is listenin at port ", port);
+})
+
+
+
+mongoose.connect("mongodb://localhost:27017/trippy_basics", (err) => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log("I'm connected to the database");
+    }
+})
+
+const hotelSchema = mongoose.Schema({
+    name: String,
+    adress: String,
+    city: String,
+    country: String,
+    stars: Number,
+    hasSpa:Boolean,
+    hasPool:Boolean,
+    priceCategory:Number,
+    date: { type: Date, default: Date.now }
+})
+
+const hotel = mongoose.model("hotel", hotelSchema)
+
+const restaurantSchema = mongoose.Schema({
+    name: String,
+    adress: String,
+    city: String,
+    country: String,
+    stars: Number,
+    cuisine:Boolean,
+    priceCategory:String,
+    date: { type: Date, default: Date.now }
+})
+
+const restaurant = mongoose.model("restaurant", restaurantSchema)
+
+app.get("/hotel", (req, res) => {
+    try {
+        const hotel = await hotel.find()
+
+        res.json(hotel)
+    } catch (err) {
+        console.error(err)
+
+        res.json({ errorMessage: "There was a problem :(" }, 500)
+    }
+
+})
