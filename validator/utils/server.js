67c6fccaa56cb83beeba9b21ffcfdaa2b1839b7e
Ezka <ezka.mehdi@gmail.com>
2021-06-16 15:25:39 +0200

    validator avec dossers classés

---
+++ validator/utils/server.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const cors = require("cors")
+const { debug } = require("./middlewares/debug")
+
+const port = 8000
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(debug)
+
+app.listen(port, () => {
+    console.log("Server is listenin at port ", port);
+})
+
+
+
+mongoose.connect("mongodb://localhost:27017/validator", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("I'm connected to the database")
+    }
+})
+
+
