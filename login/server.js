7cecd20a66a2d3fc2774488c7a2e2853446becd4
Ezka <ezka.mehdi@gmail.com>
2021-06-22 14:33:49 +0200

    debut serveur

---
+++ login/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/login", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("I'm connected to the database")
+    }
+})
+
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+
+
+const port = 8000
+app.listen(port, () => {
+    console.log("Server is listening at port ", port);
+})
+app.get('/', (req, res) => {
+    res.send('test');
+  });
